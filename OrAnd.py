d5101a03698e0ccdeb20013398e70688961d56ed
greninjalover <sumu45@gmail.com>
2026-01-28 19:03:07 +0800

    logical operators

---
+++ OrAnd.py
@@ -0,0 +1,19 @@
+a=90
+b=70
+c=100
+
+if(a>89 and c<90):
+    print("wahooo!!")
+else:
+    print("ouohwaah!!")
+
+
+
+
+
+
+
+Name="Ramcharan"
+name="ramcharan"
+if(Name != name):
+    print("not same")
