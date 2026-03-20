9589c67e66f6475119c8ffadfaace37f2ebf9c68
greninjalover <sumu45@gmail.com>
2026-03-20 18:29:16 +0800

    revision

---
+++ revision.__.py
@@ -0,0 +1,13 @@
+print("Eligibility Checker")
+print("-------------------")
+
+AGE = int(input("Tell ME Your AGE"))
+
+if AGE>=18 : 
+    print("YOU ARE unfortunately ELIGIBLE")# The world is DOOMED
+
+elif AGE <=5:
+    print(" goo goo gaa gaa")#waaaaaah
+
+else:
+    print("YOU.    ARE.      UN.   ELI.   GIBLE.  ")
