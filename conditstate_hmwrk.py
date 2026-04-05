371364e220aa1fa3b312cac10adac30c23bd6af1
greninjalover <sumu45@gmail.com>
2026-04-05 14:40:31 +0800

    homework

---
+++ conditstate_hmwrk.py
@@ -0,0 +1,9 @@
+temp= int(input("enter a temperature"))
+
+if temp<=24:
+    print("you need to wear heavy clothing")
+
+else:
+    print("you need light clothes ")
+
+# this depends on who is in the situation 24 is pretty cold for me.
