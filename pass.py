62e20e21e23f9c656850533c9613954262d1d411
greninjalover <sumu45@gmail.com>
2026-06-06 12:14:46 +0800

    keyword

---
+++ pass.py
@@ -0,0 +1,9 @@
+for i in range(20):
+    if i % 4==0:
+        print("twist")
+
+    elif i % 6==0:
+        pass
+
+    else:
+        print(i)
---
+++ werisbreak.py
@@ -0,0 +1,8 @@
+str= input("Enter A String ")
+
+for i in str:
+    if(i=='A'):
+        print("A is Found")
+        break
+    else:
+        print("A is not Found")
