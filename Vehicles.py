3ebfaca614a841776691b50762b3fda450f7f1b1
greninjalover <sumu45@gmail.com>
2026-09-26 12:43:55 +0800

    oops

---
+++ Vehicles.py
@@ -0,0 +1,10 @@
+class Vehicles:
+    def __init__(self, name, color, brand):
+        self.name=name
+        self.color=color
+        self.brand=brand
+
+v1=Vehicles("G82","Red","BMW")
+print("Name of the car is",v1.name)
+print("Color of the car is",v1.color)
+print("Brand of the car is",v1.brand)
